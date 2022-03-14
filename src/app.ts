8d7e67a4ac10cb8ace3c4707caec49fa872611f2
searchme86 <searchme86@gmail.com>
2022-03-14 16:20:01 +0900

    📦 typescript/interface

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,31 @@
-const button = document.querySelector('button');
-button?.addEventListener('click', () => {
-  console.log('clocked');
-});
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+interface Named {
+  readonly name: string;
+}
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string): void {
+    console.log(phrase);
+  }
+}
+
+// let user1: Greetable = {
+//   name: 'roh',
+//   age: 37,
+//   greet(hello: string) {
+//     console.log(`${hello}` + this.name);
+//   },
+// };
+
+// user1.greet('hi');
